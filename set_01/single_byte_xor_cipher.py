2f79d560951e6ecef24cf28180677ee6dc5088ed
Erin Browning <erinlbrowning@gmail.com>
2018-08-14 12:29:31 -0500

    challenge 4

--- set_01/single_byte_xor_cipher.py
+++ set_01/single_byte_xor_cipher.py
@@ -5,14 +5,31 @@
 # How? Devise some method for "scoring" a piece of English plaintext. Character frequency is a good metric. Evaluate each output and choose the one with the best score.
 
 import binascii 
+import string
 
-def single_byte_xor(hex_string):
-	binary = binascii.unhexlify(hex_string)
-	strings = []
-	for xor_key in range(256):
-		strings.append(''.join(chr(ord(b) ^ xor_key) for b in binary))
-	return max(strings, key=lambda string: string.count(' '))
+def single_byte_xor(hex_string, all_results):
+  binary = binascii.unhexlify(hex_string)
+  for xor_key in range(256):
+    string = ''.join(chr(ord(b) ^ xor_key) for b in binary)
+    all_results.append(string)
+  return all_results
 
-print single_byte_xor('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736')
+def check_if_printable(all_results):
+  printable_results = []
+  for result in all_results:
+    if set(result).issubset(set(string.printable)):
+      printable_results.append(result)
+  return printable_results
 
-hex_string = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
+def max_spaces(results):
+  return max(results, key=lambda string: string.count(' '))
+
+def main():
+  results = []
+  hex_string = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
+  results = single_byte_xor(hex_string, results)
+  printable = check_if_printable(results)
+  print max_spaces(printable)
+
+if __name__ == '__main__':
+  main()
