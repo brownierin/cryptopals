14d2e8a07aee24421f1e495c7db0cfe4973b57e9
Erin Browning <nope@nope.com>
2018-08-16 19:28:01 -0500

    fix method name

--- set_01/05_repeating_key_xor.py
+++ set_01/05_repeating_key_xor.py
@@ -18,7 +18,7 @@ string = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a c
 answer = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
 key = "ICE"
 
-def key_xor(string,key):
+def repeating_key_xor(string,key):
   # this will use any size key!
   solution = []
   length = len(key)
@@ -29,5 +29,5 @@ def key_xor(string,key):
     solution.append(piece)
   return ''.join(solution)
 
-my_answer = repeating_key_xor(string)
+my_answer = repeating_key_xor(string,key)
 assert my_answer == answer
