25e5fece4cdd26537fb7f6d846c8d345f4545efe
Erin Browning <nope@nope.com>
2018-08-16 19:23:38 -0500

    Clean up set 1 challenge 5 to final code

--- set_01/05_repeating_key_xor.py
+++ set_01/05_repeating_key_xor.py
@@ -17,28 +17,10 @@ import binascii
 string = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal"
 answer = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
 key = "ICE"
-solution = []
-
-# binary = ''.join(format(ord(s), 'b') for s in initial_string)
-
-def repeating_key_xor(string):
-  # eventually this should scale per size of string
-  for byte in range(0,len(string)):
-    if byte % 3 == 0:
-      piece = binascii.hexlify(chr(ord(string[byte]) ^ ord('I')))
-      solution.append(piece)
-      print "byte {0}, piece: {1}".format(byte, piece)
-    elif byte % 3 == 1:
-      piece = binascii.hexlify(chr(ord(string[byte]) ^ ord('C')))
-      solution.append(piece)
-    elif byte % 3 == 2:
-      piece = binascii.hexlify(chr(ord(string[byte]) ^ ord('E')))
-      solution.append(piece)
-    else: 
-      print "error"
-  return ''.join(solution)
 
 def key_xor(string,key):
+  # this will use any size key!
+  solution = []
   length = len(key)
   for byte in range(0,len(string)):
     modulo = byte % length
@@ -48,4 +30,4 @@ def key_xor(string,key):
   return ''.join(solution)
 
 my_answer = repeating_key_xor(string)
-# assert repeating_key_xor(string) == answer
+assert my_answer == answer
