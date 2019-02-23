7e83c2ad3d883b72d9a77fb1ac38f1bcfcb05d29
Erin Browning <erinlbrowning@gmail.com>
2019-02-23 11:55:42 -0600

    Updates to older code

--- set_01/five.py
+++ set_01/five.py
@@ -18,11 +18,11 @@ string = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a c
 answer = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
 key = "ICE"
 
-def repeating_key_xor(string,key):
+def repeating_key_xor(string, key):
   # this will use any size key!
   solution = []
   length = len(key)
-  for byte in range(0,len(string)):
+  for byte in range(0, len(string)):
     modulo = byte % length
     piece = chr(ord(string[byte]) ^ ord(key[modulo]))
     piece = binascii.hexlify(piece)
